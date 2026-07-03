b69e2cae776071bc58110702141450d6bcb30f96
aykyzy <adam.khan3@ontariotechu.net>
2026-07-03 14:48:50 -0400

    Point Google review link at the real Maps listing
    Replaces the placeholder g.page short link with the actual Google
    Maps place URL for the business, used by both the "leave a review"
    and "read more reviews on Google" links.

--- src/lib/siteConfig.ts
+++ src/lib/siteConfig.ts
@@ -9,7 +9,8 @@ export const siteConfig = {
   hours: 'Appointment only',
   instagram: '@reliablemotorsports',
   instagramUrl: 'https://www.instagram.com/reliablemotorsports/',
-  googleReviewLink: 'https://g.page/r/reliablemotorsports/review',
+  googleReviewLink:
+    'https://www.google.com/maps/place/Reliable+Motorsports/@43.7362001,-79.244474,17z/data=!3m1!4b1!4m6!3m5!1s0x89d4cf9f55480967:0x8e5eb8b2e6bafe1f!8m2!3d43.7361963!4d-79.2418991!16s%2Fg%2F11z4zjvm8m?entry=ttu&g_ep=EgoyMDI2MDYyOS4wIKXMDSoASAFQAw%3D%3D',
   brandColors: {
     bg: '#050505',
     red: '#e10600',
